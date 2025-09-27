7ef6a968596e1177e36e430e7772aad4ec010544
Ger Hobbelt <ger@hobbelt.com>
2025-09-27 22:02:11 +0200

    spelling correction

--- examples/example.cpp
+++ examples/example.cpp
@@ -140,7 +140,7 @@ int main(void)
         return result;
     });
 
-    // Compile error with message "Handler type is mismatched with URL paramters"
+    // Compile error with message "Handler type is mismatched with URL parameters"
     //CROW_ROUTE(app,"/another/<int>")
     //([](int a, int b){
     //return crow::response(500);
